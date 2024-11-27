2e049c4d4f6b494e7034dc87d77f9c42d9c9781c
hardik yadav <114461820+hardik118@users.noreply.github.com>
2024-11-27 10:44:24 +0530

    Update db.php

--- db.php
+++ db.php
@@ -2,9 +2,7 @@
 
 $servername ="mysql-14f97612-hardikyadav1182005-6846.e.aivencloud.com";
 $username ="avnadmin";
-$password = "AVNS_T22ErJJYSY5rS0kIRkR";
-$db = "defaultdb";
-$port="28115";
+
 
 // Create connection
 $con = mysqli_connect($servername, $username, $password,$db, $port);
